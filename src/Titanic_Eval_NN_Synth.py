a87939e8a9542749b421e4997bc7c7f3e3300e11
graba4 <abegovic4@etf.unsa.ba>
2019-07-31 23:31:53 +0200

    latest

--- src/Titanic_Eval_NN_Synth.py
+++ src/Titanic_Eval_NN_Synth.py
@@ -44,13 +44,97 @@ def train_and_eval(layers, size_of_layers, epochs):
     census_test.drop(columns=['Ticket'], axis=1,inplace = True)
     census_test.drop(columns=['PassengerId'], axis=1,inplace = True)
 
-    print(census_data.head())
+    
+
+
+
+    categorical = [
+        'Survived',
+        'Pclass',
+        'Sex',
+        'SibSp',
+        'Parch',
+        'Embarked'
+    ]
+
+
+    uniqitems = {}
+    for col in categorical:
+        uniqitems[col] = census_data[col].unique()
+
+    rows = []
+
+    for rowind in range(len(census_data)):
+        if rowind % 71 == 0:
+            print(rowind / len(census_data) * 100)
+        currow = census_data.iloc[rowind, :].copy()
+        for col in categorical:
+            if col != 'Survived':
+                values = str(currow[col]).split(',')
+
+                for possibleitem in uniqitems[col]:
+                    possibleitem = str(possibleitem)
+                    if possibleitem in values:
+                        currow = currow.append(pd.Series([1 / len(values)], [col + '_' + possibleitem]))
+                    else:
+                        currow = currow.append(pd.Series([0], [col + '_' + possibleitem]))
+                currow = currow.drop(col)
+        rows.append(currow.copy())
+
+    census_data = pd.DataFrame(rows)
+
+
+
+    uniqitems = {}
+    for col in categorical:
+        uniqitems[col] = census_test[col].unique()
+
+    rows = []
+
+    for rowind in range(len(census_test)):
+        if rowind % 71 == 0:
+            print(rowind / len(test_feature) * 100)
+        currow = census_test.iloc[rowind, :].copy()
+        for col in categorical:
+            if col != 'Survived':
+                values = str(currow[col]).split(',')
+
+                for possibleitem in uniqitems[col]:
+                    possibleitem = str(possibleitem)
+                    if possibleitem in values:
+                        currow = currow.append(pd.Series([1 / len(values)], [col + '_' + possibleitem]))
+                    else:
+                        currow = currow.append(pd.Series([0], [col + '_' + possibleitem]))
+                currow = currow.drop(col)
+        rows.append(currow.copy())
+
+    census_test = pd.DataFrame(rows)
+
+
+
+    df_info = pd.read_csv("titanic/gender_submission.csv", sep=",", header=0, index_col=False,
+                     engine='python');  # We load the data using Pandas
+
+    df = pd.read_csv("titanic/test.csv", sep=",", header=0, index_col=False,
+                     engine='python');  # We load the data using Pandas
+
+    info_ID = np.array(df_info['PassengerId'])
+    info_surv = np.array(df_info['Survived'])
+
+    # Actually they are in the same order, but this is the unoptimized, yet "correct" solution
+    survarray = []
+    for i in range(len(df)):
+        ID = df['PassengerId'][i]
+        survarray.append(info_surv[np.where(info_ID == ID)[0][0]])
+    df['Survived'] = survarray
+    census_test['Survived'] = survarray
 
     data_feature = census_data.drop(columns=['Survived']).copy()
     data_label = census_data['Survived'].copy()
 
     test_feature = census_test.drop(columns=['Survived']).copy()
     test_label = census_test['Survived'].copy()
+
     
     for i in range(data_label.shape[0]):
         data_label.iloc[i]=int("1" in str(data_label.iloc[i]))
